60e4759e84e251db4a4ccdb4a42c71ea45762897
kanokpan <k_sumanont@hotmail.com>
2015-08-30 13:07:08 +0700

    List view 20 items

--- app/src/main/java/joydroid/kanokpan/hello/MainActivity.java
+++ app/src/main/java/joydroid/kanokpan/hello/MainActivity.java
@@ -30,7 +30,40 @@ public class MainActivity extends AppCompatActivity {
         //Button Controller
         buttonController();
 
+        //create listview
+        createListview();
     }   // this is main method, first method to start working
+
+    private void createListview() {
+        int[] intImage= {R.drawable.traffic_01, R.drawable.traffic_02,R.drawable.traffic_03,R.drawable.traffic_04,R.drawable.traffic_05,
+                R.drawable.traffic_06,R.drawable.traffic_07, R.drawable.traffic_08,R.drawable.traffic_09,R.drawable.traffic_10,
+                R.drawable.traffic_11, R.drawable.traffic_12,R.drawable.traffic_13,R.drawable.traffic_14,R.drawable.traffic_15,
+                R.drawable.traffic_16,R.drawable.traffic_17, R.drawable.traffic_18,R.drawable.traffic_19,R.drawable.traffic_20};
+        //or  int intImage[]= {};
+        String[] strTitle = new String[20];  //reserve 20 string variables
+        strTitle[0] = "ห้ามเลี้ยวซ้าย";
+        strTitle[1] = "ห้ายเลี้ยวขวา";
+        strTitle[2] = "ตรงไป";
+        strTitle[3] = "เลี้ยวขาว";
+        strTitle[4] = "เลี้ยวซ้าย";
+        strTitle[5] = "ทางออก";
+        strTitle[6] = "ทางเข้า";
+        strTitle[7] = "ทางออก";
+        strTitle[8] = "หยุด";
+        strTitle[9] = "สูงไม่เกิน 2.5 เมตร";
+        strTitle[10] = "ทางแยก";
+        strTitle[11] = "ห้ามกลับรถ";
+        strTitle[12] = "ห้ามจอด";
+        strTitle[13] = "ทางสวน";
+        strTitle[14] = "ห้ามแซง";
+        strTitle[15] = "ทางเข้า";
+        strTitle[16] = "หยุดตรวจ";
+        strTitle[17] = "จำกัดความเร็ว";
+        strTitle[18] = "จำกัดความกว้าง";
+        strTitle[19] = "จำกัดความสูง";
+        MyAdapter objMyadaptor = new MyAdapter(MainActivity.this, intImage,strTitle );
+        trafficListView.setAdapter(objMyadaptor);
+    }   // Listview method
 
     private void buttonController() {
         aboutMeButton.setOnClickListener(new View.OnClickListener() {
